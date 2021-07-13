ca39d325f011603014c9dcea935ce07ac6eb9b63
renanmomesso <renan_momesso@hotmail.com>
2021-07-13 09:39:25 -0300

    Styled-components and tests working with jest

---
+++ src/components/Main/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+    background-color: #06092b;
+    color:#fff;
+    width: 100%;
+    height: 100vh;
+    text-align:center;
+    align-items: center;
+    justify-content: center;
+    padding:3rem;
+    display: flex;
+    flex-direction: column;
+`
+
+export const Logo = styled.img``
+
+export const Title = styled.h1``
+
+export const Description = styled.h2``
+
+export const Illustration = styled.img``;
